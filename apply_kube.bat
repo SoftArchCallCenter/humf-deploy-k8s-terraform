2451cb58e43cf926abba6716bf805b4e93e96f6e
Stickman008 <69195590+Stickman008@users.noreply.github.com>
2023-11-15 01:26:22 +0700

    fdsfsdf

--- apply_kube.bat
+++ apply_kube.bat
@@ -3,6 +3,22 @@ setlocal enabledelayedexpansion
 
 set "kubectl_command=kubectl apply -f"
 
+for /r deploy\k8s\config-secret %%f in (*.yaml) do (
+  set "file=%%f"
+  %kubectl_command% !file!
+)
+echo "Apply config and secret."
+
+for /r deploy\k8s\mysql-service %%f in (*.yaml) do (
+  set "file=%%f"
+  %kubectl_command% !file!
+)
+echo "Apply mysql."
+
+rem Wait for 30 seconds before applying the next file
+timeout /t 30 /nobreak >nul
+echo "Wait ..."
+
 for /r deploy\k8s %%f in (*.yaml) do (
   set "file=%%f"
   %kubectl_command% !file!
